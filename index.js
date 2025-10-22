a4dccafde3ee6202aae5e02d8354cee3717b448e
Bruno Adam <bruno.adam@pm.me>
2025-10-22 17:33:04 +0200

    feat: update package version to 4.1.5, improve icon exports, and add TypeScript declaration

--- index.js
+++ index.js
@@ -2,6 +2,5 @@
 import Icon from './src/Icon.js';
 export default Icon;
 
-// Also offer named exports for all generated icons (if src/icons/index.js exists)
-import * as Icons from './src/icons/index.js';
-export { Icons };
+// Export all individual icons as named exports
+export * from './src/icons/index.js';
